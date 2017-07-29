19e8cd1d4c55678061f07f36663920c8e89c1cc5
Michael Traverso <mike@traversoft.com>
2017-07-29 00:24:26 -0400

    Use the speaker photoUrl if it's available and ends in a jpg, jpeg or png.

--- lib/views/speaker_list.dart
+++ lib/views/speaker_list.dart
@@ -18,12 +18,7 @@ class SpeakerListWidget extends StatefulWidget {
 
 class _SpeakerListState extends State<SpeakerListWidget> {
   Widget buildListTile(BuildContext context, Speaker speaker) {
-    String speakerIntials;
-    if (speaker.name.split(' ').length >= 2) {
-      speakerIntials = speaker.name.split(' ')[0].substring(0, 1) + speaker.name.split(' ')[1].substring(0, 1);
-    } else {
-      speakerIntials = speaker.name.substring(0,1);
-    }
+    String speakerInitials = getCircleDetails(speaker);
     return new MergeSemantics(
       child: new ListTile(
         leading: new ExcludeSemantics(
@@ -66,9 +61,26 @@ class _SpeakerListState extends State<SpeakerListWidget> {
       body: new Scrollbar(
         child: new ListView(
             padding: new EdgeInsets.symmetric(vertical: 8.0),
-            children: listTiles //.toList(),
-            ),
+            children: listTiles),
       ),
     );
+  }
+
+  String getCircleDetails(Speaker speaker) {
+    String speakerIntials;
+    if (speaker.name.split(' ').length >= 2) {
+      speakerIntials = speaker.name.split(' ')[0].substring(0, 1) +
+          speaker.name.split(' ')[1].substring(0, 1);
+    } else {
+      speakerIntials = speaker.name.substring(0, 1);
+    }
+
+    if (speaker.photoUrl.isEmpty ||
+        !(speaker.photoUrl.endsWith("jpg") ||
+            speaker.photoUrl.endsWith("jpeg") ||
+            speaker.photoUrl.endsWith("png"))) {
+      return speakerIntials;
+    } else
+      return "";
   }
 }
