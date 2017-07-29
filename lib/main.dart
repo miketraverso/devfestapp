8f0eaf608a9f78bb23cfa024eed80ceabc9f8cef
Michael Traverso <mike@traversoft.com>
2017-07-29 00:26:55 -0400

    Some cleanup and add the SpeakerDetailsWidget route to our routes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,30 @@
+import 'dart:async';
 import 'dart:collection';
+
 import 'package:devfest_florida_app/data/schedule.dart';
 import 'package:devfest_florida_app/data/session.dart';
 import 'package:devfest_florida_app/data/speaker.dart';
 import 'package:devfest_florida_app/views/schedule_home.dart';
+import 'package:devfest_florida_app/views/speaker_detail.dart';
 import 'package:devfest_florida_app/views/speaker_list.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 List<TimeSlot> kTimeSlots = <TimeSlot>[];
 LinkedHashMap<String, Speaker> kSpeakers = new LinkedHashMap<String, Speaker>();
 LinkedHashMap<String, Session> kSessions = new LinkedHashMap<String, Session>();
 List<Schedule> kSchedules = <Schedule>[];
+Speaker kSelectedSpeaker;
 
-final ThemeData _theme = new ThemeData(
+final ThemeData kTheme = new ThemeData(
   brightness: Brightness.light,
   primarySwatch: Colors.blue,
 );
 
 final String kAppTitle = 'DevFest Florida';
+const double kPadding = 12.0;
+final Color kColorFavoriteOn = Colors.orange[500];
+final Color kColorFavoriteOff = Colors.orange[100];
 
 class DevFestApp extends StatefulWidget {
   const DevFestApp({this.onSendFeedback, Key key}) : super(key: key);
@@ -31,27 +39,19 @@ class ConfAppState extends State<DevFestApp> {
   TargetPlatform _platform;
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     var routes = <String, WidgetBuilder>{
       ScheduleHomeWidget.routeName: (BuildContext context) =>
           new ScheduleHomeWidget(),
       SpeakerListWidget.routeName: (BuildContext context) =>
-          new SpeakerListWidget()
+          new SpeakerListWidget(),
+      SpeakerDetailsWidget.routeName: (BuildContext context) =>
+          new SpeakerDetailsWidget(),
     };
 
     return new MaterialApp(
       title: kAppTitle,
-      theme: _theme.copyWith(platform: _platform),
+      theme: kTheme.copyWith(platform: _platform),
       routes: routes,
       home: new ScheduleHomeWidget(),
     );
