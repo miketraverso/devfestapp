7ea9084bd6bf4137051432d82887303c022ddfb2
Michael Traverso <mike@traversoft.com>
2017-10-08 11:51:24 -0400

    Conference-ify the Session class to make it easier to demonstrate Dart language.

--- lib/data/session.dart
+++ lib/data/session.dart
@@ -1,14 +1,16 @@
 import 'dart:collection';
 
 class Session {
-  String id;
+  int _attendeeCount = 0;		// Underscore denotes private field
   bool isFavorite = false;
-  String description;
-  String title;
-  String track = "";
-  String room = "";
+  String id, description, title, track = "", room = "";
   List<String> speakers = <String>[];
 
+  // Constructor
+  Session(this.title, this.description, this.track, this.speakers,
+      {this.room, this.isFavorite});
+
+  // Named Constructor
   Session.loadFromFireBase(String fbKey, LinkedHashMap map) {
     id = fbKey;
     for (String key in map.keys) {
@@ -31,4 +33,31 @@ class Session {
       }
     }
   }
+
+  // Getter
+  int get attendeeCounter => _attendeeCount;
+
+  // Setter
+  void set attendeeCounter(int count) {
+    this._attendeeCount = count;
+  }
+
+  @override
+  String toString() {
+    // Override toString()
+    return 'Session: $title\nSpeakers: $speakers\nTrack: $track\nExpected Attendees: $_attendeeCount';
+  }
+}
+
+void main() {
+  var session = new Session("Science: More art than science.",
+      "Wubbalubbadubdub...",
+      "DevFest Florida",
+      <String>["Rick", "Morty"]);
+
+  print('Before Attendees Set:\n$session\n\n');
+
+  session.attendeeCounter = 10;
+
+  print('After Attendees Set:\n$session');
 }
